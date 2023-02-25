9e08668731dcac9ec360cf486b646dd69619b4a7
Ameh <durojaiyeameh@gmail.com>
2023-02-25 05:49:18 -0800

    fixing last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,16 @@
  *
  * Return: Always 0.
  */
-int main(void)		
-{	
-    int r;
+int print_last_digit(int a)
+{
+	int last_digit;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	last_digit = a %  10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+
 }
