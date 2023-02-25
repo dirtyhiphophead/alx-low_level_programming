a0b2745aa2933ca21fbffbd7aa503e269d7fdd9f
Ameh <durojaiyeameh@gmail.com>
2023-02-25 06:14:02 -0800

    jack bauer minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,11 +2,33 @@
 
 /**
  * main - check the code
- *
+ * jack bauer minute
  * Return: Always 0.
  */
-int main(void)
+
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+	int i, j, k, m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
