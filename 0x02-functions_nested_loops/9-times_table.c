5b143f5bd170a2c4871d064d6b7a2cc04cabddc5
Ameh <durojaiyeameh@gmail.com>
2023-02-25 06:42:08 -0800

    print a time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,34 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * times_table - Prints the 9 times table
  *
- * Return: Always 0.
+ * Return: no return
  */
-	int main(void)
+void times_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
 	{
-	    times_table();
-	    return (0);
+
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
 	}
+}
