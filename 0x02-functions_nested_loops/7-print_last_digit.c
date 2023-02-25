075250c60fd14de12820028d8424a26a1c4faa6f
Ameh <durojaiyeameh@gmail.com>
2023-02-25 06:10:02 -0800

    fix the last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * main - check the code
- *
+ * print last digit
  * Return: Always 0.
  */
 int print_last_digit(int a)
 {
-        int last_digit;
+	int last_digit;
 
-        last_digit = a %  10;
-        if (last_digit < 0)
-        {
-                last_digit = last_digit * -1;
-        }
-        _putchar(last_digit + '0');
-        return (last_digit);
+	last_digit = a %  10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 
 }
