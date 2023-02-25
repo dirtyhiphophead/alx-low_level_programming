b8e8ee8f17b78a01afd500e0a14a63870e7e3f2e
Ameh <durojaiyeameh@gmail.com>
2023-02-25 06:03:32 -0800

    fix the last digit revert

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,14 @@
  */
 int print_last_digit(int a)
 {
-	int last_digit = a %  10;
-	if (last_digit < 0)
-	{
+        int last_digit;
 
-		last_digit = last_digit * -1;
-	}
-	_putchar(last_digit + '0');
-	return (last_digit);
+        last_digit = a %  10;
+        if (last_digit < 0)
+        {
+                last_digit = last_digit * -1;
+        }
+        _putchar(last_digit + '0');
+        return (last_digit);
 
 }
